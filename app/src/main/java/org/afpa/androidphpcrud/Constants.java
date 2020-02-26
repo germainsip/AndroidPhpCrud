432037e940bf13b9e1c20c7b1a5ff22a3989d740
Germain SIPIERE <gsipiere@gmail.com>
2020-02-26 16:53:06 +0100

    nothing important

--- app/src/main/java/org/afpa/androidphpcrud/Constants.java
+++ app/src/main/java/org/afpa/androidphpcrud/Constants.java
@@ -4,8 +4,7 @@ public class Constants {
 //    private static final String ROOT_URL = "http://172.18.204.68/androidAPI/v1/";
 
      private static final String ROOT_URL = "http://10.0.2.2/androidAPI/v1/";
-
-    // private static final String ROOT_URL = "https://dev.amorce.org/gsipiere/androidAPI/v1/";
+//     private static final String ROOT_URL = "https://dev.amorce.org/gsipiere/androidAPI/v1/";
 
 
     public static final String URL_REGISTER = ROOT_URL + "registerUser.php";
