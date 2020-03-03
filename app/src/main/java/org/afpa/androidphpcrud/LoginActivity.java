40f723335329c12719a32ddb44f30f5f79b13b90
Germain SIPIERE <gsipiere@gmail.com>
2020-03-03 08:41:20 +0100

    sharedPref

--- app/src/main/java/org/afpa/androidphpcrud/LoginActivity.java
+++ app/src/main/java/org/afpa/androidphpcrud/LoginActivity.java
@@ -2,13 +2,81 @@ package org.afpa.androidphpcrud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextUserName, getEditTextPassword;
+    private Button buttonLogin;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editTextUserName = (EditText) findViewById(R.id.editTextUserEmail);
+        getEditTextPassword = (EditText) findViewById(R.id.editTextUserPassword);
+        buttonLogin = (Button) findViewById(R.id.loginButton);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Patientez...");
+
+    }
+
+    private void userLogin() {
+        final String username = editTextUserName.getText().toString().trim();
+        final String password = getEditTextPassword.getText().toString().trim();
+
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.POST,
+                Constants.URL_LOGIN,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject obj = new JSONObject(response);
+                            if (!obj.getBoolean("error")){
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        ){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("username",username);
+                params.put("password",password);
+                return params;
+            }
+        };
+
+        RequestHandler.getInstance(this).addToRequestQueue(stringRequest);
     }
 }
